d2cc2d19ac9125f1bddf45924f9291aa6a1fede9
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-10-03 12:29:43 +0530

    Added PWA

--- brutus/settings.py
+++ brutus/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'pwa',
+    
 ]
 
 MIDDLEWARE = [
