557e92cbc1dffbbe58fb64dfa7d8beeb86d04423
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-10-05 12:48:51 +0530

    Fix Internal Server Error

--- brutus/urls.py
+++ brutus/urls.py
@@ -7,10 +7,10 @@ from landing import views
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^land/$', views.land, name='land'),
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.land, name='land'),
+    url(r'^home/$', views.home, name='home'),
     path('admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name="registration/login.html"), name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
