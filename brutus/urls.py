949eaa4ca4dcc22d757312f2cfa69d58c0ec5f8e
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-10-06 17:32:55 +0530

    Finalized login styles

--- brutus/urls.py
+++ brutus/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^$', views.land, name='land'),
     url(r'^home/$', views.home, name='home'),
     path('admin/', admin.site.urls),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
+    # url(r'^login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name="registration/login.html"), name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
