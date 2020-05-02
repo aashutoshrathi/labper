806256d2a6324269301cf66ad907f50a50432a97
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-05-02 14:08:34 +0530

    fix: 🐛 Change pipeline as per new response from login api

--- landing/pipeline.py
+++ landing/pipeline.py
@@ -6,9 +6,9 @@ def get_profile(backend, user, response, details, *args, **kwargs):
     small_url = None
     profile = Profile.objects.get_or_create(user=user)[0]
     if backend.name == "google-oauth2":
-        if response['image'].get('url'):
-            small_url = response['image'].get('url')
-            url = small_url.replace("sz=50", "sz=160")
+        if response['picture']:
+            small_url = response['picture']
+            url = small_url
     profile.avatar_small = small_url
     profile.avatar = url
     profile.save()
